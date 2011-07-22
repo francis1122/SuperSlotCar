c209eb94a13832a2ea0a9068249a66855bbc00e2
Hunter Francis <johnwilson@John-Wilsons-MacBook-Pro.local>
2011-07-21 21:59:23 -0700

    Initial Commit

---
+++ SuperSlotCar/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SuperSlotCar
+//
+//  Created by Hunter Francis on 7/21/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
